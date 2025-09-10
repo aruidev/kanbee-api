5d1f9eca723b6f84d2980666a8eb3cf049e2d38c
aruidev <arucadev@gmail.com>
2025-09-10 20:25:26 +0200

    Initial commit

---
+++ kanbeeapi/src/main/java/com/aruidev/kanbeeapi/KanbeeapiApplication.java
@@ -0,0 +1,13 @@
+package com.aruidev.kanbeeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbeeapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbeeapiApplication.class, args);
+	}
+
+}
---
+++ kanbeeapi/src/test/java/com/aruidev/kanbeeapi/KanbeeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aruidev.kanbeeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbeeapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
